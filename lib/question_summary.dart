789c9225fb04b2cf6ddf0d667ce0ce300badd66a
RON VANDA <ronvanda99@gmail.com>
2023-11-09 22:38:45 +0700

    final update result_screen

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -9,69 +9,79 @@ class QuestionSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 500,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Row(
-              children: [
-                ClipOval(
-                  child: Container(
-                    color: data['user_answer'].toString() ==
-                            data['correct_answer'].toString()
-                        ? Colors.green
-                        : Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 5,
-                    ),
-                    child: Text(
-                      ((data['question_index'] as int) + 1).toString(),
-                      style: GoogleFonts.dancingScript(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            return FittedBox(
+              fit: BoxFit.contain,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 500,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text(
-                        data['question'].toString(),
-                        style: GoogleFonts.lobster(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        data['user_answer'].toString(),
-                        style: GoogleFonts.lobster(
+                      ClipOval(
+                        child: Container(
                           color: data['user_answer'].toString() ==
                                   data['correct_answer'].toString()
                               ? Colors.green
                               : Colors.red,
-                          fontSize: 16,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
+                          ),
+                          child: Text(
+                            ((data['question_index'] as int) + 1).toString(),
+                            style: GoogleFonts.dancingScript(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
                         ),
                       ),
-                      Text(
-                        data['correct_answer'].toString(),
-                        style: GoogleFonts.lobster(
-                          color: data['user_answer'].toString() ==
-                                  data['correct_answer'].toString()
-                              ? Colors.green
-                              : Colors.red,
-                          fontSize: 16,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['question'].toString(),
+                              style: GoogleFonts.lobster(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              data['user_answer'].toString(),
+                              style: GoogleFonts.lobster(
+                                color: data['user_answer'].toString() ==
+                                        data['correct_answer'].toString()
+                                    ? Colors.green
+                                    : Colors.red,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              data['correct_answer'].toString(),
+                              style: GoogleFonts.lobster(
+                                color: data['user_answer'].toString() ==
+                                        data['correct_answer'].toString()
+                                    ? Colors.green
+                                    : Colors.red,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
+                      )
                     ],
                   ),
-                )
-              ],
+                ),
+              ),
             );
           }).toList(),
         ),
